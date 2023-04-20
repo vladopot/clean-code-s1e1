65bedde634592001af2bec758db69fa4a4ef4a3c
vladsopot@mail.ru <vladsopot@mail.ru>
2023-04-20 22:23:39 +0300

    refactor: change class names? acording BEM

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.querySelector(".main-page_incomplete-tasks");//ul of #incompleteTasks
+var completedTasksHolder=document.querySelector(".main-page_completed-tasks");//completed-tasks
 
 
 //New task list item
@@ -78,14 +78,14 @@ var addTask=function(){
 var editTask=function(){
     console.log("Edit Task...");
     console.log("Change 'edit' to 'save'");
+    console.log(this.parentNode);
 
-
-    var listItem=this.parentNode;
+    var listItem=this.parentNode; 
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
+    var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -99,7 +99,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    listItem.classList.toggle("edit-mode");
 };
 
 
